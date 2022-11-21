5d2970792bd120f0cce6efc8386b904e71aa28cb
Migue Cisterna <miguecisterna@gmail.com>
2022-11-20 21:11:41 -0300

    Ya no se pueden crear tasks vacías

---
+++ components/readTasks.js
@@ -0,0 +1,14 @@
+import { createTask } from "./addTask.js";
+import dateElement from "./dateElement.js";
+
+export const readTasks = () => {
+    const list = document.querySelector('[data-list]');
+    console.log(list);
+
+    const tasksList = JSON.parse(localStorage.getItem("tasks")) || [] ;
+
+    tasksList.forEach((task) =>{
+        list.appendChild(dateElement(task.dateFormat))
+        list.appendChild(createTask(task));
+    })
+}
